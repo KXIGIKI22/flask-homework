68f6e3b171209b913bbf6bb9d2b4a7d41b5942f4
pavlo <pavlo.kulka@gmail.com>
2023-07-03 22:12:18 +0200

    Homework 32

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+import logging
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/hello')
+@app.route('/html')
+def html():
+    app.logger.info('HTML endpoint called.')
+    return '<h1>This is an HTML response</h1>'
+
+@app.route('/json')
+def json():
+    app.logger.info('JSON endpoint called.')
+    return {'message': 'This is a JSON response'}
 def hello():
     app.logger.info('Hello endpoint called.')
     return 'Hello, world!'
